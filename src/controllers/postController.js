596bd450d5cfe563439aaa862115da2f9fbb9f31
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-08-29 21:13:06 -0300

    feat: finaliza a criacao do endpoint POST /post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,8 @@
 const postService = require('../services/postService');
+const { verifyToken } = require('../helpers/token');
+
+const MISSING_FIELDS_MSG = { message: 'Some required fields are missing' };
+const CATEGORYIDS_NOT_FOUND = { message: '"categoryIds" not found' };
 
 const getAll = async (req, res) => {
   const blogPosts = await postService.getAll();
@@ -8,7 +12,15 @@ const getAll = async (req, res) => {
 const create = async (req, res) => {
   const { title, content, categoryIds } = req.body;
 
-  const post = await postService.create(title, content, categoryIds);
+  if (!title || !content || !categoryIds) return res.status(400).json(MISSING_FIELDS_MSG);
+
+  // if (categoryIds.length === 0) return res.status(400).json(CATEGORYIDS_NOT_FOUND);
+
+  const payload = verifyToken(req.headers.authorization);
+  
+  const post = await postService.create({ title, content, categoryIds, email: payload.email });
+
+  if (!post) return res.status(400).json(CATEGORYIDS_NOT_FOUND);
   
   return res.status(201).json(post);
 };
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,4 +1,5 @@
-const { BlogPost, User, Category } = require('../database/models');
+const { BlogPost, User, Category, PostCategory } = require('../database/models');
+const { getUserByEmail } = require('./userService');
 
 /*
   Consultei o seguinte link para elaborar a função getAll:
@@ -16,8 +17,35 @@ const getAll = async () => {
   return blogPosts;
 };
 
-const create = async (title, content, categoryIds) => {
-  const post = await User.create({ title, content, categoryIds });
+const verifyIds = async (categoryIds) => {
+  const allCategories = await Category.findAll();
+
+  const ids = Object.keys(allCategories).map((id) => Number(id) + 1);
+
+  /*
+  Consultei o seguinte link para comparar os valores de 2 arrays:
+  https://pt.stackoverflow.com/questions/265446/comparar-valores-entre-arrays-javascript
+  Consultei o seguinte link para resolver o problema de lint 'Arrow function used ambiguously with a conditional expression':
+  https://stackoverflow.com/questions/41349689/how-do-you-use-an-arrow-function-with-comparison-operator-for-reduce-method
+  */
+  const checkCategories = ids.filter((id) => (categoryIds.indexOf(id) === -1 ? null : true));
+
+  return checkCategories;
+};
+
+const create = async ({ title, content, categoryIds, email }) => {
+  const userData = await getUserByEmail(email);
+  const userId = userData.dataValues.id;
+
+  const isCategoryValid = await verifyIds(categoryIds);
+  if (isCategoryValid.length === 0) return null;
+
+  const post = await BlogPost.create({ title, content, userId });
+
+  categoryIds.forEach(async (id) => { 
+    await PostCategory.create({ postId: post.id, categoryId: id });
+  });
+
   return post;
 };
 
