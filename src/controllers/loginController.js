7fa75345500a13e542b25c14f84d586b937fef01
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-11-26 15:23:25 -0300

    feat: adiciona try/catch nas funcoes do loginController

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,17 +2,23 @@ const userServices = require('../services/loginService');
 const tokenHelper = require('../helpers/token');
 
 const INVALID_FIELDS_MSG = { message: 'Invalid fields' };
+const INTERNAL_ERROR_MSG = { message: 'Internal Server Error' };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await userServices.login(email, password);
-
-  if (!user) return res.status(400).json(INVALID_FIELDS_MSG);
-
-  const token = tokenHelper.createToken({ email });
+    const user = await userServices.login(email, password);
   
-  return res.status(200).json({ token });
+    if (!user) return res.status(400).json(INVALID_FIELDS_MSG);
+  
+    const token = tokenHelper.createToken({ email });
+    
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 module.exports = {
