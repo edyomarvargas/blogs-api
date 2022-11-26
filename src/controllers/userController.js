54e387dad1dc263922aaf5a4bd5c7ee027dd432d
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-11-26 15:22:24 -0300

    feat: adiciona try/catch nas funcoes do userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,42 +1,64 @@
 const userService = require('../services/userService');
 const tokenHelper = require('../helpers/token');
 
+const INTERNAL_ERROR_MSG = { message: 'Internal Server Error' };
+
 const create = async (req, res) => {
-  const { displayName, email, password, image } = req.body;
+  try {
+    const { displayName, email, password, image } = req.body;
 
-  const findUser = await userService.getUserByEmail(email);
-  if (findUser) return res.status(409).json({ message: 'User already registered' });
-
-  await userService.create(displayName, email, password, image);
-
-  const token = tokenHelper.createToken({ displayName, email, image });
+    const findUser = await userService.getUserByEmail(email);
+    if (findUser) return res.status(409).json({ message: 'User already registered' });
   
-  return res.status(201).json({ token });
+    await userService.create(displayName, email, password, image);
+  
+    const token = tokenHelper.createToken({ displayName, email, image });
+    
+    return res.status(201).json({ token });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 const getAll = async (req, res) => {
-  const users = await userService.getAll();
-  return res.status(200).json(users);
+  try {
+    const users = await userService.getAll();
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 const findByPk = async (req, res) => {
-  const { id } = req.params;
-
-  const user = await userService.findByPk(id);
-
-  if (!user) return res.status(404).json({ message: 'User does not exist' });
-
-  return res.status(200).json(user);
+  try {
+    const { id } = req.params;
+  
+    const user = await userService.findByPk(id);
+  
+    if (!user) return res.status(404).json({ message: 'User does not exist' });
+  
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 const remove = async (req, res) => {
-  const { email: userEmail } = tokenHelper.verifyToken(req.headers.authorization);
-
-  const userData = await userService.getUserByEmail(userEmail);
-
-  await userService.remove(userData.dataValues.id);
-
-  return res.status(204).end();
+  try {
+    const { email: userEmail } = tokenHelper.verifyToken(req.headers.authorization);
+  
+    const userData = await userService.getUserByEmail(userEmail);
+  
+    await userService.remove(userData.dataValues.id);
+  
+    return res.status(204).end();
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 module.exports = {
