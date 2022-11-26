7e741cad3fa6a1e50f4eb1101cc4f4c40912bf32
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-11-26 15:10:31 -0300

    feat: adiciona try/catch nas funcoes do postController

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -6,75 +6,104 @@ const MISSING_FIELDS_MSG = { message: 'Some required fields are missing' };
 const CATEGORYIDS_NOT_FOUND = { message: '"categoryIds" not found' };
 const POST_NOT_FOUND = { message: 'Post does not exist' };
 const UNAUTHORIZED_USER_MSG = { message: 'Unauthorized user' };
+const INTERNAL_ERROR_MSG = { message: 'Internal Server Error' };
 
 const getAll = async (req, res) => {
-  const blogPosts = await postService.getAll();
-  return res.status(200).json(blogPosts);
+  try {
+    const blogPosts = await postService.getAll();
+    return res.status(200).json(blogPosts);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 const create = async (req, res) => {
-  const { title, content, categoryIds } = req.body;
-
-  if (!title || !content || !categoryIds) return res.status(400).json(MISSING_FIELDS_MSG);
-
-  const payload = verifyToken(req.headers.authorization);
+  try {
+    const { title, content, categoryIds } = req.body;
   
-  const post = await postService.create({ title, content, categoryIds, email: payload.email });
-
-  if (!post) return res.status(400).json(CATEGORYIDS_NOT_FOUND);
+    const payload = verifyToken(req.headers.authorization);
+    
+    const post = await postService.create({ title, content, categoryIds, email: payload.email });
   
-  return res.status(201).json(post);
+    if (!post) return res.status(400).json(CATEGORYIDS_NOT_FOUND);
+    
+    return res.status(201).json(post);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 const findByPk = async (req, res) => {
-  const { id } = req.params;
-
-  const post = await postService.findByPk(id);
-
-  if (!post) return res.status(404).json(POST_NOT_FOUND);
-  return res.status(200).json(post);
+  try {
+    const { id } = req.params;
+  
+    const post = await postService.findByPk(id);
+  
+    if (!post) return res.status(404).json(POST_NOT_FOUND);
+    return res.status(200).json(post);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 const update = async (req, res) => {
-  const { id: postId } = req.params;
-  const { title, content } = req.body;
-
-  const userData = verifyToken(req.headers.authorization);
-
-  if (!title || !content) return res.status(400).json(MISSING_FIELDS_MSG);
-
-  const isUserValid = await validateUser(postId, userData);
-
-  if (!isUserValid) return res.status(401).json(UNAUTHORIZED_USER_MSG);
-
-  await postService.update({ id: postId, title, content });
-
-  const updatedPost = await postService.findByPk(postId);
-
-  return res.status(200).json(updatedPost);
+  try {
+    const { id: postId } = req.params;
+    const { title, content } = req.body;
+  
+    const userData = verifyToken(req.headers.authorization);
+  
+    if (!title || !content) return res.status(400).json(MISSING_FIELDS_MSG);
+  
+    const isUserValid = await validateUser(postId, userData);
+  
+    if (!isUserValid) return res.status(401).json(UNAUTHORIZED_USER_MSG);
+  
+    await postService.update({ id: postId, title, content });
+  
+    const updatedPost = await postService.findByPk(postId);
+  
+    return res.status(200).json(updatedPost);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 const remove = async (req, res) => {
-  const { id } = req.params;
-  const userData = verifyToken(req.headers.authorization);
-
-  const findPost = await postService.findByPk(id);
-  if (!findPost) return res.status(404).json(POST_NOT_FOUND);
-
-  const isUserValid = await validateUser(id, userData);
-  if (!isUserValid) return res.status(401).json(UNAUTHORIZED_USER_MSG);
-
-  await postService.remove(id);
-
-  return res.status(204).end();
+  try {
+    const { id } = req.params;
+    const userData = verifyToken(req.headers.authorization);
+  
+    const findPost = await postService.findByPk(id);
+    if (!findPost) return res.status(404).json(POST_NOT_FOUND);
+  
+    const isUserValid = await validateUser(id, userData);
+    if (!isUserValid) return res.status(401).json(UNAUTHORIZED_USER_MSG);
+  
+    await postService.remove(id);
+  
+    return res.status(204).end();
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 const search = async (req, res) => {
-  const { q: query } = req.query;
+  try {
+    const { q: query } = req.query;
 
-  const queryResult = await postService.search(query);
-
-  return res.status(200).json(queryResult);
+    const queryResult = await postService.search(query);
+  
+    return res.status(200).json(queryResult); 
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 module.exports = {
