c50eb40a000fc23f0c57a1bf465a25e1e233b62f
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-11-26 14:52:57 -0300

    feat: adiciona try/catch nas funcoes do categoryController

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,22 +1,30 @@
 const categoryService = require('../services/categoryService');
 
 const NAME_REQUIRED_MSG = { message: '"name" is required' };
+const INTERNAL_ERROR_MSG = { message: 'Internal Server Error' };
 
 const create = async (req, res) => {
-  const { name } = req.body;
+  try {
+    const { name } = req.body;
 
-  if (!name) return res.status(400).json(NAME_REQUIRED_MSG);
-
-  const result = await categoryService.create(name);
-
-  console.log('result', result.id);
+    if (!name) return res.status(400).json(NAME_REQUIRED_MSG);
   
-  return res.status(201).json(result);
+    const result = await categoryService.create(name);  
+    return res.status(201).json(result);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
-const getAll = async (req, res) => {
-  const categories = await categoryService.getAll();
-  return res.status(200).json(categories);
+const getAll = async (_req, res) => {
+  try {
+    const categories = await categoryService.getAll();
+    return res.status(200).json(categories); 
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(INTERNAL_ERROR_MSG);
+  }
 };
 
 module.exports = {
