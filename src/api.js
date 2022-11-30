01a303ac2ee8556e365521790e1993c086c8009f
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-11-29 21:15:19 -0300

    feat: implementa a documentacao da api no endpoint /documentation

--- src/api.js
+++ src/api.js
@@ -1,19 +1,18 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const routes = require('./routes');
 const { tokenValidation } = require('./middlewares/authentication');
 
-// ...
 const app = express();
 
 app.use(express.json());
+
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/login', routes.loginRoute);
 app.use('/user', routes.userRoute);
 app.use('/categories', tokenValidation, routes.categoryRoute);
 app.use('/post', tokenValidation, routes.postRoute);
 
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
