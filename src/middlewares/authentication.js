ad5425a8b8b67c5c9e30d6775a6f108819c65769
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-11-29 22:36:51 -0300

    style: remove comentarios devido a criacao de uma funcao separada para a funcionalidade descrita no comentario

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,19 +1,15 @@
 const tokenHelper = require('../helpers/token');
+const { getCorrectTokenFormat } = require('../helpers/formatToken');
 
 const tokenValidation = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
 
-  /* Como o swagger envia a 'authorization' no formato 'Bearer <token>', a linha abaixo
-  faz um split para separar em um array a palavra 'Bearer e o token */
-  const token = authorization.split(' ');
+  const token = getCorrectTokenFormat(authorization);
+
   try {
-    /* Como a const token pode ser um array com uma ou duas posições, a linha abaixo
-    garante que sempre será passado o token (último elemento do array) para a função
-    de verificação */
-    const tokenData = tokenHelper.verifyToken(token[token.length - 1]);
-    console.log('tokenData', tokenData);
+    tokenHelper.verifyToken(token);
     next();
   } catch (error) {
     console.log(error.message);
