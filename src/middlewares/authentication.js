8ab490be3b956bb95c57fdf040153a8c38135a38
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-11-29 21:18:38 -0300

    refactor: altera a funcao de verificacao do token de acordo com a requisicao recebida no swagger

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -5,11 +5,18 @@ const tokenValidation = (req, res, next) => {
 
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
 
+  /* Como o swagger envia a 'authorization' no formato 'Bearer <token>', a linha abaixo
+  faz um split para separar em um array a palavra 'Bearer e o token */
+  const token = authorization.split(' ');
   try {
-    const tokenData = tokenHelper.verifyToken(authorization);
+    /* Como a const token pode ser um array com uma ou duas posições, a linha abaixo
+    garante que sempre será passado o token (último elemento do array) para a função
+    de verificação */
+    const tokenData = tokenHelper.verifyToken(token[token.length - 1]);
     console.log('tokenData', tokenData);
     next();
   } catch (error) {
+    console.log(error.message);
     return res.status(401).json({ message: 'Expired or invalid token' });
   }
 };
