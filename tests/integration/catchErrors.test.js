21de0ef055cd18b00ef4f7e97beff929e896a362
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-12-15 00:07:07 -0300

    test: implementa testes para os casos de erro ao criar ou remover usuarios na rota /user

--- tests/integration/catchErrors.test.js
+++ tests/integration/catchErrors.test.js
@@ -71,5 +71,60 @@ describe.only('Testa se as funções lançam erros', () => {
         expect(response.body.message).to.be.equals(INTERNAL_ERROR_MSG);
       });
     });
+
+    describe('lança um erro ao cadastrar uma pessoa usuária', () => {
+      const newUser = {
+        displayName: 'Brett Wiltshire',
+        email: 'brett@email.com',
+        password: '123456',
+        image:
+        'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
+      };
+
+      before(async () => {
+        const stubThrows = { message: INTERNAL_ERROR_MSG };
+        sinon.stub(User, 'create').throws(stubThrows);
+
+        const { token } = loginResponse.body;
+        response = await chai.request(app)
+        .post('/user')
+        .send(newUser)
+        .set('authorization', token);
+      });
+
+      after(() => {
+        User.create.restore();
+      });
+
+      it('A requisição deve retornar código de status 500', () => {
+        expect(response).to.have.status(500);
+      });
+      it('A requisição deve retornar a mensagem "Internal Server Error"', () => {
+        expect(response.body.message).to.be.equals(INTERNAL_ERROR_MSG);
+      });
+    });
+
+    describe('lança um erro ao remover uma pessoa usuária', () => {
+      before(async () => {
+        const stubThrows = { message: INTERNAL_ERROR_MSG };
+        sinon.stub(User, 'destroy').throws(stubThrows);
+
+        const { token } = loginResponse.body;
+        response = await chai.request(app)
+        .delete('/user/me')
+        .set('authorization', token);
+      });
+
+      after(() => {
+        User.destroy.restore();
+      });
+
+      it('A requisição deve retornar código de status 500', () => {
+        expect(response).to.have.status(500);
+      });
+      it('A requisição deve retornar a mensagem "Internal Server Error"', () => {
+        expect(response.body.message).to.be.equals(INTERNAL_ERROR_MSG);
+      });
+    });
   });
 });
